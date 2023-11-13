19830d508c1e007afbcebb63a23fec96b3cd0713
RonMeier <48986706+RonMeier@users.noreply.github.com>
2023-11-13 16:20:12 +0100

    Update OTAWebUpdater.ino

--- espupload/OTAWebUpdater.ino
+++ espupload/OTAWebUpdater.ino
@@ -8,42 +8,14 @@ const char* password = "pwd";
 
 WebServer server(80);
 
-const char* serverIndex =
-"<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
-"<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
- "<input type='file' name='update'>"
- "<input type='submit' value='Update'>"
- "</form>"
- "<div id='prg'>progress: 0%</div>"
- "<script>"
-  "$('form').submit(function(e){"
-  "e.preventDefault();"
-  "var form = $('#upload_form')[0];"
-  "var data = new FormData(form);"
-  " $.ajax({"
-  "url: '/update',"
-  "type: 'POST',"
-  "data: data,"
-  "contentType: false,"
-  "processData:false,"
-  "xhr: function() {"
-  "var xhr = new window.XMLHttpRequest();"
-  "xhr.upload.addEventListener('progress', function(evt) {"
-  "if (evt.lengthComputable) {"
-  "var per = evt.loaded / evt.total;"
-  "$('#prg').html('progress: ' + Math.round(per*100) + '%');"
-  "}"
-  "}, false);"
-  "return xhr;"
-  "},"
-  "success:function(d, s) {"
-  "console.log('success!')"
- "},"
- "error: function (a, b, c) {"
- "}"
- "});"
- "});"
- "</script>";
+const char* serverIndex = "<!DOCTYPE HTML><html><head>"                             
+    "<meta http-equiv='Content-Type' content='text/html; charset=utf-8'>"
+    "<meta name='viewport' content='width=device-width, initial-scale=1.1'>"
+    "<h2>FW update</h2><hr>"
+    "<form method='POST' action='/update' enctype='multipart/form-data'>"
+    "<input type='file' name='update'><br><br>"
+    "<input type='submit' value='Update'>"
+    "</head></html>";    
 
 void setup(void) {
   Serial.begin(115200);
